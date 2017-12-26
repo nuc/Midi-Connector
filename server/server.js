b4c322e67ecc4898b4f54c84dbbf4dc743a9ab9a
Georgios Giannoutsos Barkas <contact@nuc.gr>
2017-12-26 18:06:09 +0100

    Server cleanup

--- server/server.js
+++ server/server.js
@@ -1,33 +1,35 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const Koa = require('koa')
+const Router = require('koa-router')
+const cors = require('@koa/cors')
 
-const app = express()
 const aconnect = require('./lib/aconnect.js')
 
-app.use(bodyParser.json())
+const app = new Koa()
+const router = new Router()
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE')
+// app.use(cors)
+
+router.get('/midi-devices', async (ctx, next) => {
+  const devices = await aconnect.getMidiDevices()
+  console.log(devices)
+  ctx.response.status = 200
+  ctx.body = devices
   next()
 })
 
-app.get('/midi-devices', (req, res) => {
-  aconnect.getMidiDevices().then(output =>
-    res.send(output)
-  )
-})
+app.use(router.routes())
 
-app.post('/connect', (req, res) => {
-  const { sourceId, targetId } = req.body
-  aconnect.connectDevices({ sourceId, targetId }).then(response => res.send(response))
-})
+module.exports = app.listen(3000)
 
-app.delete('/disconnect-all', (req, res) => {
-  aconnect.disconnectAllDevices().then(response => res.send(response))
-})
+// app.post('/connect', (req, res) => {
+//   const { sourceId, targetId } = req.body
+//   aconnect.connectDevices({ sourceId, targetId }).then(response => res.send(response))
+// })
 
-app.listen(3000, () => {
-  console.log('App listening on http://localhost:3000')
-})
+// app.delete('/disconnect-all', (req, res) => {
+//   aconnect.disconnectAllDevices().then(response => res.send(response))
+// })
+
+// app.listen(3000, () => {
+//   console.log('App listening on http://localhost:3000')
+// })
