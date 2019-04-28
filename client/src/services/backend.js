5cbd5280ac620d523dbe9686379e29bc0b6ae461
Georgios Giannoutsos Barkas <contact@nuc.gr>
2019-04-28 15:36:30 +0200

    Remove hardcoded URL

--- client/src/services/backend.js
+++ client/src/services/backend.js
@@ -1,25 +1,25 @@
 import request from 'superagent'
 
 // const API_URL = 'http://localhost:3000'
-const API_URL = 'http://raspberrypi.local:3000'
+// const API_URL = 'http://raspberrypi.local:3000'
 
 const backend = {
   fetchMidiConnections: () =>
     request
-      .get(`${API_URL}/midi-devices`)
+      .get('/midi-devices')
       .set('Content-Type', 'application/json'),
 
   createConnection: ({ sourceId, targetId }) => {
     const body = { sourceId, targetId }
     return request
-      .post(`${API_URL}/connect`)
+      .post('/connect')
       .set('Content-Type', 'application/json')
       .send(JSON.stringify(body))
   },
 
   disconnectAll: () =>
     request
-      .del(`${API_URL}/disconnect-all`)
+      .del('/disconnect-all')
       .set('Content-Type', 'application/json')
 }
 
